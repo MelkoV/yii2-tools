7fcc79e8c269c0861820f1fc38f802cc4f7b2c29
AntonSmorodin <anton@smorodin.me>
2016-09-20 12:32:47 +0500

    add queuejs

---
+++ queuejs/QueueJs.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace melkov\components\queuejs;
+
+use melkov\components\queuejs\lib\QueueJsException;
+use yii\base\Component;
+
+class QueueJs extends Component
+{
+    public $host = 'localhost';
+    public $port = '8124';
+    public $key;
+    public $prefix;
+
+    public function init()
+    {
+        if ($this->prefix) {
+            $this->prefix .= '_';
+        }
+        parent::init();
+    }
+
+    public function getConfig()
+    {
+        $array = array(
+            'host' => $this->host,
+            'port' => $this->port,
+            'key' => $this->key,
+            'prefix' => $this->prefix,
+        );
+        return $array;
+    }
+
+    public function addJob($name, $params = array())
+    {
+        return $this->query('addJob', array('name' => $this->prefix . $name, 'params' => urlencode(json_encode($params))));
+    }
+
+    public function addWorker($name, $command)
+    {
+        return $this->query('addWorker', array('name' => $this->prefix . $name, 'command' => $command));
+    }
+
+    public function clearWorkerJob($name)
+    {
+        return $this->query('clearWorkerJob', array('name' => $this->prefix . $name));
+    }
+
+    public function deleteWorker($name)
+    {
+        return $this->query('deleteWorker', array('name' => $this->prefix . $name));
+    }
+
+    public function getStat()
+    {
+        $response = $this->query('getStat');
+        return $response['message'];
+    }
+
+    private function query($method, $params = array())
+    {
+        $params['action'] = $method;
+        $response = $this->request($params);
+
+        if (!$response) {
+            throw new QueueJsException(\Yii::t('queueJs', 'Server not available'));
+        }
+
+        return $response;
+    }
+
+    public function getCount($name)
+    {
+        $result = $this->getStat();
+        foreach ($result as $qname => $value) {
+            if (strpos($qname, $name) !== false) {
+                return $value["status"];
+            }
+        }
+        return -1;
+    }
+
+    private function request($params)
+    {
+        $params['signature'] = md5($this->arrayToString($params) . $this->key);
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        if (!$socket) {
+            return false;
+        }
+        if (!@socket_connect($socket, $this->host, $this->port)) {
+            return false;
+        }
+        socket_write($socket, json_encode($params));
+
+        $result = '';
+
+        while ($read = socket_read($socket, 1024)) {
+            $result .= $read;
+        }
+        socket_close($socket);
+
+        return json_decode($result, true);
+    }
+
+    private function arrayToString($array)
+    {
+        ksort($array);
+
+        $return = '';
+
+        if (is_array($array)) {
+            foreach ($array as $val) {
+                if (is_array($val)) {
+                    $return .= $this->arrayToString($val);
+                } else {
+                    $return .= $val;
+                }
+            }
+        } else {
+            $return .= $array;
+        }
+
+        return $return;
+    }
+} 
---
+++ queuejs/lib/QueueJsException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace melkov\components\queuejs\lib;
+
+use yii\base\Exception;
+
+class QueueJsException extends Exception
+{
+
+}
