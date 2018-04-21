8433d4f217d39acddbdddd991a60457be37187d2
Anton Smorodin <anton@berrysoft.ru>
2018-04-21 20:04:28 +0500

    fix format

--- helpers/PhoneHelper.php
+++ helpers/PhoneHelper.php
@@ -29,15 +29,36 @@ class PhoneHelper
     }
 
     /**
+	 * @deprecated 
      * @param string $number
      *
      * @return string
      */
     public static function asString($number)
     {
+        return self::format($number);
+    }
+	
+	/**
+     * @param string $number
+     * @param string $format
+     * @param bool $international
+     * @return string
+     */
+	public static function format($number, $format = "+x (xxx) xxx-xx-xx", $international = true)
+    {
         if ($string = self::normalNumber($number)) {
-            return "+" . substr($string, 0, 1) . " (" . substr($string, 1, 3) . ") " . substr($string, 4, 3) . "-"
-            . substr($string, 7);
+            if (!$international) {
+                $string{0} = "8";
+            }
+            $cnt = substr_count($format, "x");
+            $len = $cnt < 11 ? $cnt : 11;
+            $string = strrev($string);
+
+            for ($i  = $len; $i > 0; $i--) {
+                $format{strpos($format, "x")} = $string{$i - 1};
+            }
+            return $format;
         } else {
             return $number;
         }
